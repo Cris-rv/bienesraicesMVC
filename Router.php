2f67e7381db56fc958b7f77d7471a318c0dbb4f7
Cristopher <rivasbritoc@gmail.com>
2025-06-11 23:50:10 -0600

    Cambios a router

--- Router.php
+++ Router.php
@@ -27,16 +27,16 @@ class Router {
         $urlActual = $_SERVER['PATH_INFO'] ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
 
-        // if($metodo === 'GET') {
-        //     $fn = $this->rutasGET[$urlActual] ?? null; // Mediante el codigo en $urlActual filtramos dentro de nuestro arreglo, para traer la fn asociada a cada url ($key)
-        // } else {
-        //     $fn = $this->rutasPOST[$urlActual] ?? null;
-        // }
-
-        // Si la url actual es la misma que la url protegida y no esta autenticado redireccionamos al usuario
-        if(in_array($urlActual, $rutas_protegidas) && !$auth) {
-            header('Location: /');
+        if($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null; // Mediante el codigo en $urlActual filtramos dentro de nuestro arreglo, para traer la fn asociada a cada url ($key)
+        } else {
+            $fn = $this->rutasPOST[$urlActual] ?? null;
         }
+
+        // // Si la url actual es la misma que la url protegida y no esta autenticado redireccionamos al usuario
+        // if(in_array($urlActual, $rutas_protegidas) && !$auth) {
+        //     header('Location: /');
+        // }
 
         if($fn) {
             // Si la URL existe y hay una funcion asociada a la ruta que el usuario esta visitando entonces:
