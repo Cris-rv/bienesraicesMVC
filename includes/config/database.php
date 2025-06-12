ce5d36fdf810f5f5a6212bc5c2781c795784d416
Cristopher <rivasbritoc@gmail.com>
2025-06-11 20:04:41 -0600

    Cambios en database

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB() : mysqli {
-    $db = new mysqli('127.0.0.1', 'root', 'root', 'Bienesraices_CRUD'); // Forma orientada a objetos
+    $db = new mysqli('190.106.222.203', '417786', 'Crisrb1218$', 'bienesraicesmvc_cristopher'); // Forma orientada a objetos
 
     if(!$db) {
         echo "Error no se pudo conectar";
